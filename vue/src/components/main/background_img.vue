8c154dd8407bab5e6f02a936915349bd399f017f
DeveloperRyou <DeveloperRyou@gmail.com>
2021-12-07 16:30:15 +0900

    add main/header

---
+++ vue/src/components/main/background_img.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class=background_img>
+        <img alt="background_img" src="@/assets/background_img_N.png">
+    </div>
+    <!--<a href='https://.pngtree.com/so/사업'>사업 png에서 .pngtree.com/</a>-->
+</template>
+<script>
+export default {
+    name: 'background_img'
+}
+</script>
+
+<style lang="css">
+.background_img{
+    position: relative;
+    height: 100%;
+}
+.background_img img{
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    width: 50%;
+    min-width: 25rem;
+    object-fit: cover;
+}
+</style>
---
+++ vue/src/components/main/title_box.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="title-box"> 
+        <div class="pretitle">
+            <p>Hello, I’m</p>
+        </div>
+        <div class="title neonText">
+            <p>Developer</p>
+            <p>Ryou</p>
+        </div>
+        <div class="descript">
+            <p>안녕하세요.</p>
+            <p>Web / Algorithm / AI 에 흥미있는</p>
+            <p>DeveloperRyou의 Portfolio + Blog입니다.</p>
+        </div>
+        <div class="button">
+            <a class="ui inverted basic button">More</a>
+        </div>
+    </div>  
+</template>
+<script>
+export default {
+    name: 'title_box'
+}
+</script>
+
+<style lang="css">
+.title-box{
+  position: absolute;
+  top: 20%;
+  left: 10%;
+  text-align: left;
+  color: #fff;
+}
+
+.title-box .pretitle{
+    font-weight: 700;
+    font-size: 2rem;
+}
+.title-box .title{
+  font-size: 6rem;
+  letter-spacing: 0.3rem;
+  margin-top: 1.5rem;
+}
+.title-box .title p{
+  line-height: 7rem;
+}
+.title-box .neonText{
+  font-family: neon;
+  color: #fff;
+  text-shadow: 0 0 1px #50CFD6, 0 0 3px #50CFD6, 0 0 10px #50CFD6, 0 0 20px #50CFD6;
+}
+.title-box .descript{
+    margin-top: 2rem;
+    font-size: 1.2rem;
+}
+.title-box .button{
+    margin-top:1rem;
+}
+.title-box .button a{
+    width: 13rem;
+}
+</style>
