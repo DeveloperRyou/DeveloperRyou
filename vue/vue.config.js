e20ae4c31967f932b8a58942c8d0476dc97c4513
DeveloperRyou <DeveloperRyou@gmail.com>
2021-12-08 21:58:15 +0900

    add @ to point static folder

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -1,14 +1,23 @@
+const path = require('path');
+
 module.exports = { 
-    devServer: { // api 요청 처리 위치
-      proxy: { 
-        '/api': { 
-          target: 'http://localhost:3000/api',
-          changeOrigin: true, 
-          pathRewrite: { 
-            '^/api': ''
-          } 
+  configureWebpack: {
+    resolve: {
+        alias: {
+            '@': path.join(__dirname, 'src/')
+        }
+      }
+  },
+  devServer: { // api 요청 처리 위치
+    proxy: { 
+      '/api': { 
+        target: 'http://localhost:3000/api',
+        changeOrigin: true, 
+        pathRewrite: { 
+          '^/api': ''
         } 
       } 
-    },
-    outputDir: '../public',  // 배포 파일의 위치
-  }
+    } 
+  },
+  outputDir: '../public',  // 배포 파일의 위치
+}
