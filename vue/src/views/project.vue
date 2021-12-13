bca33d739a92389c780161cc44501817cc4799f2
DeveloperRyou <DeveloperRyou@gmail.com>
2021-12-13 20:00:49 +0900

    make draft of project

--- vue/src/views/project.vue
+++ vue/src/views/project.vue
@@ -1,11 +1,26 @@
 <template lang="html">
-    <h1>
-        Developer Ryou - Project
-    </h1>
+    <Navigator></Navigator>
+    <section>
+        <article class="projects">
+            <div class="container">
+                <Projects></Projects>
+            </div>
+        </article>
+    </section>
+    <footer>
+        <Footer></Footer>
+    </footer>
 </template>
 <script>
+import Navigator from '@/components/navigator'
+import Projects from '@/components/main/projects'
+import Footer from '@/components/footer'
 export default {
+    name: 'project',
+    components: {Navigator,Projects,Footer}
 }
 </script>
 
-<style lang="css"></style>
+<style lang="css">
+
+</style>
